4b9650fc2a1e4fddd0f27e5454bf0df9cb94645a
virat59 <virat59@gmail.com>
2015-05-13 17:48:01 +0530

    Commit 8

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+		alert('Device Ready');
         app.receivedEvent('deviceready');
         var pushNotification = window.plugins.pushNotification;
         pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"824841663931","ecb":"app.onNotificationGCM"});
@@ -55,7 +56,7 @@ var app = {
         alert('Callback Success! Result = '+result)
     },
     errorHandler:function(error) {
-        alert(error);
+        alert(' Error : ' + error);
     },
     onNotificationGCM: function(e) {
         switch( e.event )
